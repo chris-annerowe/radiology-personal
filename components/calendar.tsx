a0ef70058a9ce3be728e56d71db094776d7ef76a
unknown <chris-anne.rowe@aye-tech.com>
2024-05-08 16:19:42 -0500

    bare calendar datetime picker functionality

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -4,34 +4,34 @@ import ReactCalendar from 'react-calendar'
 import React, { useState } from 'react'
 import { add, format } from 'date-fns'
 
-interface DateType {
-    justDate: Date | null
-    dateTime: Date | null
-}
+// interface DateType {
+//     justDate: Date | null
+//     dateTime: Date | null
+// }
 
 const Calendar = () => {
-    const [date, setDate] = useState<DateType>({
-        justDate: null,
-        dateTime: null
-    })
+    // const [date, setDate] = useState<DateType>({
+    //     justDate: null,
+    //     dateTime: null
+    // })
 
-    const getTimes = () => {
-        if(!date.justDate) return
+    // const getTimes = () => {
+    //     if(!date.justDate) return
 
-        const {justDate} = date
-        const startTime = add(justDate, {hours: 9}) //sets opening time to 9:00AM
-        const end = add(justDate, {hours: 17})  //sets closing time to 5:00PM
-        const interval = 30 //in minutes. Select date/time in 30 min intervals
+    //     const {justDate} = date
+    //     const startTime = add(justDate, {hours: 9}) //sets opening time to 9:00AM
+    //     const end = add(justDate, {hours: 17})  //sets closing time to 5:00PM
+    //     const interval = 30 //in minutes. Select date/time in 30 min intervals
     
-        const times = []
-        for(let i=startTime; i<=end; i=add(justDate,{minutes:interval})){
-            times.push(i)
-        }
+    //     const times = []
+    //     for(let i=startTime; i<=end; i=add(justDate,{minutes:interval})){
+    //         times.push(i)
+    //     }
 
-        return times
-    }
+    //     return times
+    // }
 
-    const times = getTimes()
+    // const times = getTimes()
 
     return (
       <div className='h-screen flex flex-col justify-center items-center'>
@@ -39,9 +39,9 @@ const Calendar = () => {
             className="REACT-CALENDAR p-2"
             minDate={new Date()}
             view='month'
-            onClickDay={(date)=>setDate((prev) => ({...prev, justDate: date}))}
+            onClickDay={(date)=>{console.log(date)}}
         />
-        {date.justDate ? (
+        {/* {date.justDate ? (
             <div className='flex gap-4'>
             {times?.map((time, i) => (
                 <div key={`time-${i}`} className='rounded-sm bg-gray-100 p-2'>
@@ -52,7 +52,7 @@ const Calendar = () => {
             ))}
             </div>
         )
-        : null}
+        : null} */}
       </div>
   )
 }
