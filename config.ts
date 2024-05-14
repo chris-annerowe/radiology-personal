c5fd60172b42acd82aeea85d2fe7ddc77e908b2b
unknown <chris-anne.rowe@aye-tech.com>
2024-05-14 13:19:47 -0500

    Config file

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const OPENING_HOURS = 9 //in 24hr format(9:00AM)
+export const CLOSING_HOURS = 17 //in 24hr format(5:00PM)
+export const BUSINESS_HOURS_INTERVAL = 30 //in mins (Can select time slot for every 30 mins)
