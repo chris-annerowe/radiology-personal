03d1a853d9dd2d66954592efc462c96d567b97e4
unknown <chris-anne.rowe@aye-tech.com>
2025-12-16 17:38:10 -0500

    Update type mismatch

--- app/(dashboard)/dashboard/accessioning/page.tsx
+++ app/(dashboard)/dashboard/accessioning/page.tsx
@@ -50,25 +50,27 @@ export default async function Accessioning({
         if(typeof searchParam === 'string'){
             const resp = await findPatientById(searchParam)
             
-            patient.patient_id = resp?.patient_id
-            patient.first_name = resp?.first_name
-            patient.last_name = resp?.last_name
-            patient.other_name = resp?.other_name
-            patient.title = resp?.title
-            patient.dob = resp?.dob
-            patient.age = resp?.age
-            patient.sex = resp?.sex
-            patient.nationality = resp?.nationality
-            patient.address_1 = resp?.address_1
-            patient.address_2 = resp?.address_2
-            patient.city = resp?.city
-            patient.parish = resp?.parish
-            patient.telephone_1 = resp?.telephone_1
-            patient.telephone_2 = resp?.telephone_2
-            patient.cellular = resp?.cellular
-            patient.email = resp?.email
-            patient.id_type = resp?.id_type
-            patient.idnum = resp?.idnum
+            if(resp){
+                patient.patient_id = resp?.patient_id
+                patient.first_name = resp?.first_name
+                patient.last_name = resp?.last_name
+                patient.other_name = resp?.other_name
+                patient.title = resp?.title
+                patient.dob = resp?.dob
+                patient.age = resp?.age
+                patient.sex = resp?.sex
+                patient.nationality = resp?.nationality
+                patient.address_1 = resp?.address_1
+                patient.address_2 = resp?.address_2
+                patient.city = resp?.city
+                patient.parish = resp?.parish
+                patient.telephone_1 = resp?.telephone_1
+                patient.telephone_2 = resp?.telephone_2
+                patient.cellular = resp?.cellular
+                patient.email = resp?.email
+                patient.id_type = resp?.id_type
+                patient.idnum = resp?.idnum
+            }
 
             console.log("Patient to accession from patient list ",patient)
         }
