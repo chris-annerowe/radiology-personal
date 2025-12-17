7fc0469183210ce62a316657d677615ad3012048
unknown <chris-anne.rowe@aye-tech.com>
2025-12-16 19:20:18 -0500

    Comment out unused api route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,42 +1,42 @@
-import { NextResponse } from "next/server";
-import { db } from '@/lib/db';
-import { hash } from 'bcrypt';
+// import { NextResponse } from "next/server";
+// import { db } from '@/lib/db';
+// import { hash } from 'bcrypt';
 
-export async function POST(req: Request) {
-    try{
-        const body = await req.json();
-        const { username, email, password } = body;
+// export async function POST(req: Request) {
+//     try{
+//         const body = await req.json();
+//         const { username, email, password } = body;
 
-        //check if email already exists
-        const existingUserByEmail = await db.user.findUnique({
-            where: {email:email}
-        });
-        if(existingUserByEmail){
-            return NextResponse.json({ user: null, message: 'User with this email already exists'}, {status: 409})
-        }
+//         //check if email already exists
+//         const existingUserByEmail = await db.user.findUnique({
+//             where: {email:email}
+//         });
+//         if(existingUserByEmail){
+//             return NextResponse.json({ user: null, message: 'User with this email already exists'}, {status: 409})
+//         }
 
-        //check if username already exists
-        const existingUserByUsername = await db.user.findUnique({
-            where: {username:username}
-        });
-        if(existingUserByUsername){
-            return NextResponse.json({ user: null, message: 'User with this username already exists'}, {status: 409})
-        }
+//         //check if username already exists
+//         const existingUserByUsername = await db.user.findUnique({
+//             where: {username:username}
+//         });
+//         if(existingUserByUsername){
+//             return NextResponse.json({ user: null, message: 'User with this username already exists'}, {status: 409})
+//         }
 
-        const hashedPassword = await hash(password, 10)
-        const newUser = await db.user.create({
-            data: {
-                username,
-                email,
-                password: hashedPassword
-            }
-        })
+//         const hashedPassword = await hash(password, 10)
+//         const newUser = await db.user.create({
+//             data: {
+//                 username,
+//                 email,
+//                 password: hashedPassword
+//             }
+//         })
 
-        //Remove password from response object(do not send password inside response).
-        const {password: newUserPassword, ...rest} = newUser;
+//         //Remove password from response object(do not send password inside response).
+//         const {password: newUserPassword, ...rest} = newUser;
 
-        return NextResponse.json({user: rest, message: 'User created successfully'}, {status: 201});
-    }catch(e){
-        return NextResponse.json({message: 'Something went wrong!'}, {status: 500});
-    }
-}
+//         return NextResponse.json({user: rest, message: 'User created successfully'}, {status: 201});
+//     }catch(e){
+//         return NextResponse.json({message: 'Something went wrong!'}, {status: 500});
+//     }
+// }
